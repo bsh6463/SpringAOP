bd2c498c41b70a31e95259a3cb0829d92ba95cf4
bsh6463 <bsh9510@gmail.com>
2022-01-05 23:43:22 +0900

    220105 SpringAop - Pointcut

---
+++ src/main/java/hello/aop/member/annotation/MethodAop.java
@@ -0,0 +1,12 @@
+package hello.aop.member.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME) //Runtime때까지 annotation유지
+public @interface MethodAop {
+    String value();
+}
